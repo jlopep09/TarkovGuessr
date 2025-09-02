596a09f7614cf272ef9ba97d3b0434f46d3ec5cf
jlopep09 <jlopep09@estudiantes.unileon.es>
2025-09-02 15:56:44 +0200

    Finished microservices structure and updated readme

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='flex flex-row justify-center align-middle items-center text-amber-300'>App</div>
+    <div className='flex flex-col justify-center align-middle min-w-lvw text-center items-center'>
+
+      <h1>Tarkov Guessr</h1>
+      <p>Tarkov guessr is a game.</p>
+      <p></p>
+    </div>
   )
 }
 
