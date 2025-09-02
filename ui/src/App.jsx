ce38eee18a6841c1836c8a1890a952c3a8b65c87
jlopep09 <jlopep09@estudiantes.unileon.es>
2025-09-02 15:48:41 +0200

    Removed template files and updated tailwindcss version

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,40 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from 'primereact/button'; 
-import { SpeedDial } from 'primereact/speeddial';
-import { Toast } from 'primereact/toast';
-import BasicDemo from './BasicDemo';
-import MultiAxisDemo from './MultiAxisDemo';
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='text-red-200'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <MultiAxisDemo></MultiAxisDemo>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='flex flex-row justify-center align-middle items-center text-amber-300'>App</div>
   )
 }
 
--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -1,11 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <PrimeReactProvider>
-      <App />
-    </PrimeReactProvider>
+    <App />
 )
