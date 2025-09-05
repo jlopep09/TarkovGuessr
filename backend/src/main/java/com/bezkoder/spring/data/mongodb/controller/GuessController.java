71677dc76c5674dfb896936bb20cb740afa537a4
jlopep09 <jlopep09@estudiantes.unileon.es>
2025-09-05 11:26:28 +0200

    fix(updated cors config)

--- backend/src/main/java/com/bezkoder/spring/data/mongodb/controller/GuessController.java
+++ backend/src/main/java/com/bezkoder/spring/data/mongodb/controller/GuessController.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class GuessController {
